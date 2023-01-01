20c83a05565b8399abf572f76398287aa735dc97
Joakim Strandberg <joakim@mequinox.se>
2023-01-01 17:19:30 +0100

    Updated main.adb

--- main.adb
+++ main.adb
@@ -1,8 +1,19 @@
+with Ada.Text_IO;
 with Ada.Command_Line;
 with Ac21;
 with Ac22;
 
 procedure Main is
+   procedure Put_Line (Item : String) renames Ada.Text_IO.Put_Line;
+
+   procedure Print_Help is
+   begin
+      Put_Line ("On Linux or Mac OS X:");
+      Put_Line ("Try 'aoc tests', 'aoc 2021' or 'aoc 2022'");
+      Put_Line ("On Windows or FreeDOS:");
+      Put_Line ("Try 'aoc.exe tests', 'aoc.exe 2021' or 'aoc.exe 2022'");
+   end Print_Help;
+
    function Argument_Count return Natural is
    begin
       return Ada.Command_Line.Argument_Count;
@@ -19,5 +30,7 @@ begin
       elsif Argument (1) = "2022" then
          Ac22.Solve_Puzzle;
       end if;
+   else
+      Print_Help;
    end if;
 end Main;
